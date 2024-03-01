15dc4c185ff2f89e663230462dd89481f7486747
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2024-03-01 07:17:04 +0000

    5. Using dynamic data

--- app.py
+++ app.py
@@ -2,13 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scienist',
+        'location': 'Dehli, India',
+        'salary': 'Rs. 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'New Yourk, USA',
+        'salary': 'Rs. 20,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Backend Engineer',
+        'location': 'LA, USA',
+        'salary': 'Rs. 25,00,000'
+    },
+]
+
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', companyName='HYUNDAI', jobs=JOBS)
 
 
 if (__name__ == '__main__'):
   app.run(host='0.0.0.0', debug=True)
 
-  #finished on 57.27
+  #finished on 1.18.27
