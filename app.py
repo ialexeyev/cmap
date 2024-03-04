5cb4b983291e61d2fc9c8f050a7823f46159a320
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2024-03-04 04:47:49 +0000

    7. Adding requirements (Flask and gunicorn)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -32,6 +32,10 @@ JOBS = [
 @app.route('/')
 def hello_world():
   return render_template('home.html', companyName='HYUNDAI', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobjs():
+  return jsonify(JOBS)
 
 
 if (__name__ == '__main__'):
